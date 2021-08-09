0b630291e1eb1ea2912ab4dc30fbd40f2ab403ac
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-08-09 19:05:05 -0300

    add sidebar.html, mod settings, urls #53

--- bsin/bsin/settings.py
+++ bsin/bsin/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'bsin.urls'
 
+TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
