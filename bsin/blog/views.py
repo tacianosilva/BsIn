8159d944438ac488c6942d355a6a1eb5581273f1
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-08-09 21:20:31 -0300

    mod views #53

--- bsin/blog/views.py
+++ bsin/blog/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 class PostList(generic.ListView):
@@ -6,3 +7,26 @@ class PostList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 2 #paginação
 
+def post_detail(request, slug):
+    template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
