57f763eae1092712351fb992bb4e09bbd174e5ff
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-08-09 21:23:18 -0300

    mod views, add forms.py #53

--- bsin/blog/views.py
+++ bsin/blog/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import CommentForm
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
