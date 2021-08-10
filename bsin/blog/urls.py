3e48c417b12fd4096cefd32506d03aa4c9713d04
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-08-09 22:23:50 -0300

    mod urls #53

--- bsin/blog/urls.py
+++ bsin/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('create', views.PostCreate.as_view(), name='post_create'),
+    path('update/<int:pk>', views.PostUpdate.as_view(), name='post_update'),
 ]
