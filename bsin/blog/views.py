158dbd414d5c608cb5e40f351ec55b06b094ca09
JFmaia <jfmaia741@gmail.com>
2021-08-12 16:30:30 -0300

    modified file #46
    Tirando view de submit login pois não estar funcionado.

--- bsin/blog/views.py
+++ bsin/blog/views.py
@@ -96,14 +96,3 @@ def login_user(request):
     return render(request, 'login/login.html')
 
 
-def submit_login(request):
-    if request.POST:
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(username = username, password = password)
-        if user is not None:
-            login(request, user)
-            return redirect('index.html')
-        else:
-            messages.success(request,"Usuário ou senha inválido! Tente novamente.")
-    return redirect('login/login.html')
