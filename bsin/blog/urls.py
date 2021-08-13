179ddff454ccc2381dceb2f4eb972bd8a8ef8fce
JFmaia <jfmaia741@gmail.com>
2021-08-13 09:38:31 -0300

    modified file #46
    Adicionando urly para index e para a view de deslogar.

--- bsin/blog/urls.py
+++ bsin/blog/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('index/',views.index, name = 'index'),
     path('login/', views.login_user, name = 'login_user'),
     path('cadastrar/', views.cadastrar_user, name = 'cadastrar_user'),
+    path('deslogar/', views.deslogar_usuario, name='logout'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('create', views.PostCreate.as_view(), name='post_create'),
     path('update/<int:pk>', views.PostUpdate.as_view(), name='post_update'),
